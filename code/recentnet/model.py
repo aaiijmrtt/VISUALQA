98548e26c1839e4547f2e41fd4a89a4ef668e928
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2017-05-25 14:41:28 +0530

    recentnet: tested model

--- code/recentnet/model.py
+++ code/recentnet/model.py
@@ -1,5 +1,5 @@
 import sys, configparser, signal, datetime
-import tensorflow as tf, numpy as np
+import tensorflow as tf, numpy as np, scipy.misc, scipy.ndimage
 import tensorflow.contrib.slim.nets as nets
 import recentnet
 
@@ -18,7 +18,7 @@ def featureextractor(placeholder):
 	return subparts['vgg_19/conv5/conv5_4']
 
 def extractfeature(session, model, filename):
-	return session.run(model['featureextractor'], feed_dict = {model['rawimage']: np.load(filename).reshape(1, 224, 224, 3)})
+	return session.run(model['featureextractor'], feed_dict = {model['rawimage']: scipy.misc.imresize(scipy.ndimage.imread(filename), [224, 224, 3]).reshape(1, 224, 224, 3)}).reshape(14, 14, 512)
 
 def feed(model, config, filename, session):
 	batch, length = config.getint('global', 'batchsize'), config.getint('global', 'timesize')
@@ -37,7 +37,7 @@ def feed(model, config, filename, session):
 			images, questions, answers = list(), list(), list()
 
 def run(model, config, session, summary, filename, train):
-	iters, freq, time, saves, total = config.getint('global', 'iterations') if train else 1, config.getint('global', 'frequency'), config.getint('global', 'timesize'), config.get('global', 'output'), 0.
+	iters, freq, time, saves, total = config.getint('global', 'iterations') if train == 'train' else 1, config.getint('global', 'frequency'), config.getint('global', 'timesize'), config.get('global', 'output'), 0.
 
 	for i in xrange(iters):
 		for ii, feeddict in enumerate(feed(model, config, filename, session)):
@@ -48,16 +48,16 @@ def run(model, config, session, summary, filename, train):
 					print datetime.datetime.now(), 'iteration', i, 'batch', ii, 'loss:', val, total
 			elif train == 'dev' or train == 'test':
 				val = session.run(model['answer'], feed_dict = feeddict)
-				exps, vals, outs = feeddict[model['labels']], [x[0] for x in val], [x[1] for x in val]
+				exps, vals, outs = feeddict[model['labels']], val.values, val.indices
 				for exp, out in zip(exps, outs):
-					if exp == out: total += 1
+					if exp in out: total += 1
 
 	return total
 
 def handler(signum, stack):
 	print datetime.datetime.now(), 'terminating execution'
 	print datetime.datetime.now(), 'saving model'
-	tf.train.Saver(variables + newvariables).save(sess, config.get('global', 'save'))
+	tf.train.Saver(variables + [model[name] for name in newvariables]).save(sess, config.get('global', 'save'))
 	sys.exit()
 
 if __name__ == '__main__':
@@ -70,21 +70,21 @@ if __name__ == '__main__':
 		model = dict()
 		model['rawimage'] = tf.placeholder(tf.float32, [1, 224, 224, 3])
 		model['featureextractor'] = featureextractor(model['rawimage'])
-		variables, newvariables = tf.contrib.slim.get_variables_to_restore(), [model[name] for name in ['wE', 'Fq', 'Fv', 'h_', 'wU', 'wV', 'wW', 'wH', 'wR', 'gs']]
+		variables, newvariables = tf.contrib.slim.get_variables_to_restore(), ['wE', 'Fq', 'Fv', 'h_', 'wU', 'wV', 'wW', 'wH', 'wR', 'gs']
 
 		if sys.argv[2] == 'init':
 			sess.run(tf.global_variables_initializer())
 			tf.train.Saver(variables).restore(sess, config.get('global', 'preload'))
 			model.update(recentnet.create(config['recentnet']))
-			sess.run(tf.initialize_variables(newvariables))
+			sess.run(tf.variables_initializer([model[name] for name in newvariables]))
 		else:
 			model.update(recentnet.create(config['recentnet']))
-			sess.run(tf.initialize_all_variables())
-			tf.train.Saver(variables + newvariables).restore(sess, config.get('global', 'load'))
+			sess.run(tf.global_variables_initializer())
+			tf.train.Saver(variables + [model[name] for name in newvariables]).restore(sess, config.get('global', 'load'))
 			print datetime.datetime.now(), 'running model'
 			summary = tf.summary.FileWriter(config.get('global', 'logs'), sess.graph)
 			returnvalue = run(model, config, sess, summary, '%s/%s' %(config.get('global', 'data'), sys.argv[2]), sys.argv[2])
 			print datetime.datetime.now(), 'returned value', returnvalue
 
 		print datetime.datetime.now(), 'saving model'
-		tf.train.Saver(variables + newvariables).save(sess, config.get('global', 'save'))
+		tf.train.Saver(variables + [model[name] for name in newvariables]).save(sess, config.get('global', 'save'))
